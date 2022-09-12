0f79ff53ce79e85a6f988e89c7d568e1aa103d7f
Modhi2o2 <modi77177@gmail.com>
2022-09-12 22:37:01 +0300

    done

---
+++ My project (3)/Assets/NewBehaviourScript.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnMouseDrag()
+    {
+        Vector2 mousePos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        Vector2 playerPos = Camera.main.ScreenToWorldPoint(mousePos);
+        transform.position = playerPos;
+    }
+}
